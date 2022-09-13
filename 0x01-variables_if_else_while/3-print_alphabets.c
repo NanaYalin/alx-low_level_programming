da181dc1789a38186a994786c09c3dd6a3506800
NanaYalin <lindaattara49@gmail.com>
2022-09-13 07:43:38 +0000

    fix betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,16 @@
  */
 int main(void)
 {
-        char i, j;
-     
-       for (i = 'a'; i <= 'z'; i++)
-       { 
-         putchar(i);
-        }
-         for (j = 'A'; j <= 'Z'; j++)
-	 {
-	       putchar(j);
-         }
-         putchar('\n');
-         return (0);
-}
+    char i, j;
 
+    for (i = 'a'; i <= 'z'; i++)
+    { 
+        putchar(i);
+    }
+    for (j = 'A'; j <= 'Z'; j++)
+    {
+        putchar(j);
+    }
+    putchar('\n');
+    return (0);
+}
