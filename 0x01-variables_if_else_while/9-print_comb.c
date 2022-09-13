96405fbcf7b45aa8db770a2a3058298fe60f10f8
Linda Attara <lindaattara49@gmail.com>
2022-09-12 19:47:11 -0700

    combination of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - prints all possible combinations of single-digit number
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{	
+	int i;
+
+          for (i = 48; i <= 57; i++)
+
+	{
+		putchar(1);
+		if(i != 57);
+
+		{
+			putchar(',');
+			putchar('');
+		}
+		putchar('\n')
+		return (0);
+}	
+
