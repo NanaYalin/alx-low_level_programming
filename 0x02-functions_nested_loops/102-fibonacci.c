245813e7aba8337fd7df067f9e89e345efd6b048
Linda Attara <lindaattara49@gmail.com>
2022-09-15 16:15:40 -0700

    prints the first 50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *  main - prints out first 50
+ *  fibonacci suit numbers
+ *  Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long x, y;
+
+
+	x = 0;
+	y = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		unsigned long sum;
+
+		sum = x + y;
+		x = y;
+		y = sum;
+		printf("%lu", sum);
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	{
+		return (0);
+}
