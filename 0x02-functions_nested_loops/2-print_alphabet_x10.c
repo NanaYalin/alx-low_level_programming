74975f70ea1332cc2284ae2ab47615577154fd10
Linda Attara <lindaattara49@gmail.com>
2022-09-15 00:38:30 -0700

    10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * print_alphabet_x10 - print alphabets 10 times
  * Return: void
  */
@@ -10,8 +9,8 @@ void print_alphabet_x10(void)
 {
 
 	int count;
-	count = 10;
 
+	count = 10;
 	while (count > 0)
 	{
 		char x;
