bd7a19fe7e5c23bb72ecf26c059015cfaa2f16ff
Linda Attara <lindaattara49@gmail.com>
2022-10-11 20:22:54 -0700

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,39 +11,43 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int nl, ol, i;
-	dog_t *dog;
+	dog_t *new_dog;
+	char *name_copy;
+	char *owner_copy;
+	int name_length = 0;
+	int owner_length = 0;
+	int i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	while (name[name_length])
+		name_length++;
+	while (owner[owner_length])
+		owner_length++;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
-	for (nl = 0; name[nl]; nl++)
-		;
-	nl++;
-	dog->name = malloc(nl * sizeof(char));
-	if (dog->name == NULL)
+	name_copy = malloc(name_length + 1);
+	if (name_copy == NULL)
 	{
-		free(dog);
+		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; i < nl; i++)
-		dog->name[i] = name[i];
-	dog->age = age;
-	for (ol = 0; owner[ol]; ol++)
-		;
-	ol++;
-	dog->owner = malloc(ol * sizeof(char));
-	if (dog->owner == NULL)
+	for (i = 0; name[i]; i++)
+		name_copy[i] = name[i];
+	name_copy[i] = '\0';
+	owner_copy = malloc(owner_length + 1);
+	if (owner_copy == NULL)
 	{
-		free(dog->name);
-		free(dog);
+		free(new_dog);
+		free(name_copy);
 		return (NULL);
 	}
-	for (i = 0; i < ol; i++)
-		dog->owner[i] = owner[i];
-	return (dog);
+	for (i = 0; owner[i]; i++)
+		owner_copy[i] = owner[i];
+	owner_copy[i] = '\0';
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+	return (new_dog);
 }
-	
-	
