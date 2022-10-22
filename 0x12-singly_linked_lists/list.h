a0683e0b4260cb55117f25569ee85a2890bc1eb9
Linda Attara <lindaattara49@gmail.com>
2022-10-21 18:20:27 -0700

    prints all the elements of list_t

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
 
 #include <stdlib.h>
 
@@ -26,4 +26,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 
-#endif /* LISTS_H */
+#endif /* LIST_H */
