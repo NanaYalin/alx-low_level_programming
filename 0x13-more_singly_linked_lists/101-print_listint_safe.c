bc9cb8d30bcbef794dbdd97b67c8afffc05ee9c7
Linda Attara <lindaattara49@gmail.com>
2022-10-26 06:59:39 -0700

    function that prints a listint_t

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -19,8 +19,10 @@ size_t looped_listint_len(const listint_t *head)
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
 
+
 	if (head == NULL || head->next == NULL)
 		return (0);
+
 
 	tortoise = head->next;
 	hare = (head->next)->next;
