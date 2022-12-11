26eb6f29cb99d3fd4bf9db7aac91582c3917b1cf
Linda Attara <lindaattara49@gmail.com>
2022-12-10 18:08:13 -0800

    elements of a disint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,18 +4,26 @@
  * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: The head of the dlistint_t list.
  *
- * Return: The number of nodes in the list.
+ * Return: number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
 
-	while (h)
+	int counter = 0;
+
+	if (h == NULL)
+		return (counter);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		counter++;
 	}
 
-	return (nodes);
+	return (counter);
 }
